061f90d20c8bc16452c546447ea6d09a5f1b81af
Jed Davis <jld@xlerb.net>
2019-12-19 08:48:24 -0700

    Abstract the I/O shell for part 2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,5 +1,7 @@
 use itertools::iterate;
+use std::env::args;
 use std::io::{stdin, BufRead};
+use std::process::exit;
 
 fn fuel_req(mass: u32) -> u32 {
     (mass / 3).saturating_sub(2)
@@ -12,7 +14,9 @@ fn fix_fuel(mass: u32) -> u32 {
         .sum()
 }
 
-fn main() {
+fn tmain<F>(fuel_fn: F)
+    where F: Fn(u32) -> u32
+{
     let stdin = stdin();
     
     let total: u32 =
@@ -20,10 +24,21 @@ fn main() {
              .lines()
              .map(|rline| rline.expect("I/O error reading stdin"))
              .map(|line| u32::from_str_radix(&line, 10).unwrap())
-             .map(fuel_req)
+             .map(fuel_fn)
              .sum();
 
     println!("{}", total);
+}
+
+fn main() {
+    match args().nth(1).as_ref().map(|s| s as &str) { // sigh
+        Some("1") => tmain(fuel_req),
+        Some("2") => tmain(fix_fuel),
+        _ => {
+            eprintln!("Usage: {} <part #>", args().nth(0).unwrap());
+            exit(1)
+        }
+    }
 }
 
 #[cfg(test)]
