5c7786f60781c02d330ce8478f4f82f35ee7ff75
Jed Davis <jld@xlerb.net>
2019-12-19 13:33:41 -0700

    day03: The rest of the spec tests.

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -62,4 +62,18 @@ mod test {
             .collect();
         assert_eq!(isects, vec![Point { x: 6, y: 5 }, Point { x: 3, y: 3 }]);
     }
+
+    #[test]
+    fn spec_line1() {
+        assert_eq!(part1("R75,D30,R83,U83,L12,D49,R71,U7,L72",
+                         "U62,R66,U55,R34,D71,R55,D58,R83"),
+                   159);
+    }
+
+    #[test]
+    fn spec_line2() {
+        assert_eq!(part1("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+                         "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"),
+                   135);
+    }
 }
