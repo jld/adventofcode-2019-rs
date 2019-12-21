80680658fb70e5af5967642a003fbb20fd085b02
Jed Davis <jld@xlerb.net>
2019-12-20 19:29:14 -0700

    day05: Expose diagnostic system ID as argument.

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,12 +1,14 @@
+use std::env::args;
 use std::io::{stdin, prelude::*};
+use std::str::FromStr;
 
 use intcode::{Computer, Device, Word, IOError};
 
-struct DiagDev;
+struct DiagDev(Word);
 
 impl Device for DiagDev {
     fn input(&mut self) -> Result<Word, IOError> {
-        Ok(1)
+        Ok(self.0)
     }
     fn output(&mut self, val: Word) -> Result<(), IOError> {
         Ok(println!("{}", val))
@@ -14,8 +16,9 @@ impl Device for DiagDev {
 }
 
 fn main() {
+    let sys_id = args().nth(1).map(|s| Word::from_str(&s).unwrap()).unwrap_or(1);
     let stdin = stdin();
     let prog = stdin.lock().lines().next().expect("no input").expect("I/O error reading stdin");
     let mut cpu = Computer::from_str(&prog).expect("parse error");
-    cpu.run(&mut DiagDev).expect("runtime error");
+    cpu.run(&mut DiagDev(sys_id)).expect("runtime error");
 }
