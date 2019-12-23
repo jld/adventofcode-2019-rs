ea57fc69e3856696a21fd2aab7300e78644f4b84
Jed Davis <jld@xlerb.net>
2019-12-22 20:58:17 -0700

    day13: Interactive mode, to check out some of the edge cases.
    The input, like the comment says, is line-based because I'm too lazy
    to put the tty into cbreak mode.  (I'm sure there's a crate for it.)
    Yes, program input is still stdin; opening /dev/tty isn't hard, and the
    Rust stdlib's stdin/stdout can't be used as a boxed abstract I/O thing
    anyway because of the lock borrowing stuff.

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,5 +1,8 @@
 use std::collections::HashMap;
-use std::io::{stdin, prelude::*};
+use std::env::args;
+use std::fs::OpenOptions;
+use std::io::{stdin, prelude::*, BufReader, BufWriter};
+use std::ops::Drop;
 
 use intcode::{Computer, Device, Word, IOError};
 
@@ -30,20 +33,35 @@ struct CmdBuf {
     buf: Vec<Word>,
 }
 
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum Cmd {
+    None,
+    Draw(Word, Word, Tile),
+    Status(Word)
+}
+
 impl CmdBuf {
     fn new() -> Self { Self { buf: vec![] } }
 
-    fn handle(&mut self, val: Word) -> Result<Option<(Word, Word, Tile)>, IOError> {
+    fn handle(&mut self, val: Word) -> Result<Cmd, IOError> {
         debug_assert!(self.buf.len() < 3);
         self.buf.push(val);
         Ok(if self.buf.len() == 3 {
             let x = self.buf[0];
             let y = self.buf[1];
-            let t = Tile::from_word(self.buf[2]).ok_or(IOError)?;
+            let data = self.buf[2];
             self.buf.clear();
-            Some((x, y, t))
+
+            if x == -1 && y == 0 {
+                Cmd::Status(data)
+            } else if x < 0 || y < 0 {
+                return Err(IOError);
+            } else {
+                let tile = Tile::from_word(data).ok_or(IOError)?;
+                Cmd::Draw(x, y, tile)
+            }
         } else {
-            None
+            Cmd::None
         })
     }
 }
@@ -67,9 +85,112 @@ impl Device for ScreenDev {
     fn input(&mut self) -> Result<Word, IOError> { Err(IOError) }
 
     fn output(&mut self, val: Word) -> Result<(), IOError> {
-        Ok(if let Some((x, y, tile)) = self.cmd.handle(val)? {
-            self.tiles.insert((x, y), tile);
+        Ok(match self.cmd.handle(val)? {
+            Cmd::Draw(x, y, tile) => { self.tiles.insert((x, y), tile); }
+            Cmd::None => (),
+            _ => return Err(IOError),
         })
+    }
+}
+
+struct TermDev {
+    cmd: CmdBuf,
+    line_in: Box<dyn BufRead>,
+    vt_out: Box<dyn Write>,
+    cursor: Option<(Word, Word)>,
+    status: Word,
+    lowest: Word,
+}
+
+type TermAction = std::io::Result<()>;
+
+impl TermDev {
+    fn new(r: impl BufRead + 'static, w: impl Write + 'static) -> Self {
+        Self {
+            cmd: CmdBuf::new(),
+            line_in: Box::new(r),
+            vt_out: Box::new(w),
+            cursor: None,
+            status: -1,
+            lowest: -1,
+        }
+    }
+
+    fn cls(&mut self) -> TermAction {
+        write!(&mut self.vt_out, "\x1b[2J")
+    }
+
+    fn goto(&mut self, x: Word, y: Word) -> TermAction {
+        debug_assert!(x >= 0 && y >= 0);
+        if let Some((cx, cy)) = self.cursor {
+            if cx == x && cy == y {
+                return Ok(())
+            }
+        } else {
+            self.cls()?;
+        }
+        write!(&mut self.vt_out, "\x1b[{};{}H", y+1, x+1)?;
+        self.cursor = Some((x, y));
+        Ok(())
+    }
+
+    fn put_text(&mut self, x: Word, y: Word, text: &str, width: Word) -> TermAction {
+        self.goto(x, y)?;
+        write!(&mut self.vt_out, "{}", text)?;
+        self.cursor.as_mut().unwrap().0 += width;
+        Ok(())
+    }
+    
+    fn write_status(&mut self, prompt: &str) -> TermAction {
+        self.goto(0, self.lowest + 1)?;
+        write!(&mut self.vt_out, "{}{}\x1b[K", self.status, prompt)?;
+        self.vt_out.flush()?;
+        self.cursor.as_mut().unwrap().0 = -1;
+        Ok(())
+    }
+}
+
+impl Device for TermDev {
+    fn output(&mut self, val: Word) -> Result<(), IOError> {
+        Ok(match self.cmd.handle(val)? {
+            Cmd::None => (),
+            Cmd::Status(status) => self.status = status,
+            Cmd::Draw(x, y, tile) => {
+                self.lowest = self.lowest.max(y);
+                let text = match tile {
+                    Tile::Empty => ".",
+                    Tile::Wall => "#",
+                    Tile::Block => "$",
+                    Tile::HPad => "=",
+                    Tile::Ball => "*",
+                };
+                self.put_text(x, y, text, 1).expect("tty write error");
+            }
+        })
+    }
+
+    fn input(&mut self) -> Result<Word, IOError> {
+        self.write_status("> ").expect("tty write error");
+        // I'm too lazy to put the tty into cbreak mode, so line input it is.
+        let mut line = String::new();
+        self.line_in.read_line(&mut line).expect("tty read error");
+        let joy = match line.chars().next().unwrap().to_ascii_lowercase() {
+            'l' | 'w' => -1,
+            'r' | 'e' => 1,
+            'n' | 'h' => 0,
+            _ => panic!("unknown direction {:?}", line.trim())
+        };
+        Ok(joy)
+    }
+}
+
+impl Drop for TermDev {
+    fn drop(&mut self) {
+        // Ignore all error here because double panic isn't useful.
+        let _ = self.write_status("");
+        // Move the cursor back down in case there's a panic message under the status line
+        let _ = write!(&mut self.vt_out, "\n\n");
+        let _ = self.vt_out.flush();
     }
 }
 
@@ -78,8 +199,22 @@ fn main() {
     let stdin = stdin();
     let prog = stdin.lock().lines().next().expect("no input").expect("I/O error reading stdin");
     let mut cpu = Computer::from_str(&prog).expect("parse error");
+    std::mem::drop(stdin);
 
-    let mut dev = ScreenDev::new();
-    cpu.run(&mut dev).expect("runtime error");
-    println!("{}", dev.tiles.iter().filter(|&(_xy, &t)| t == Tile::Block).count());
+    let cmd = args().nth(1).expect("need argument: blocks | play");
+    if cmd == "blocks" {
+        let mut dev = ScreenDev::new();
+        cpu.run(&mut dev).expect("runtime error");
+        println!("{}", dev.tiles.iter().filter(|&(_xy, &t)| t == Tile::Block).count());
+    } else if cmd == "play" {
+        let tty_in = OpenOptions::new().read(true).open("/dev/tty")
+                                                  .expect("error opening /dev/tty for read");
+        let tty_out = OpenOptions::new().write(true).open("/dev/tty")
+                                                    .expect("error opening /dev/tty for write");
+        let mut dev = TermDev::new(BufReader::new(tty_in), BufWriter::new(tty_out));
+        cpu.write(0, 2).unwrap();
+        cpu.run(&mut dev).expect("runtime error");
+    } else {
+        panic!("bad command {}", cmd);
+    }
 }
