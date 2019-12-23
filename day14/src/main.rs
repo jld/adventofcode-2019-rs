a748bdd051f85ba3bae73dfa42a55bafdabe1c08
Jed Davis <jld@xlerb.net>
2019-12-23 07:21:09 -0700

    day14: Parsing, yay.  (Ugh.)

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::str::FromStr;
 
 type Amount = u64;
 
@@ -27,6 +28,28 @@ struct Bench {
 
 fn div_round_up(n: Amount, d: Amount) -> Amount {
     (n + d - 1) / d
+}
+
+impl Thing {
+    fn from_str(s: &str) -> Self {
+        let mut it = s.splitn(2, " ");
+        let t0 = it.next().expect("Thing::from_str: empty?");
+        let t1 = it.next().expect("Thing::from_str: no space?");
+        let n = Amount::from_str(t0).expect("Thing::from_str: bad number");
+        Self { quantity: n, substance: t1.to_owned() }
+    }
+}
+
+impl Reaction {
+    fn from_str(s: &str) -> Self {
+        let mut io = s.splitn(2, " => ");
+        let i = io.next().expect("Reaction::from_str: empty?");
+        let o = io.next().expect("Reaction::from_str: no arrow?");
+        Self {
+            output: Thing::from_str(o),
+            inputs: i.split(", ").map(|s| Thing::from_str(s)).collect()
+        }
+    }
 }
 
 impl Book {
@@ -73,6 +96,10 @@ impl Bench {
             self.obtain(book, scaled_input);
         }
     }
+
+    fn get_fuel(&mut self, book: &Book) {
+        self.obtain(book, Thing { quantity: 1, substance: "FUEL".to_owned() })
+    }
 }
 
 fn main() {
@@ -111,7 +138,7 @@ mod test {
               (&[(7, "A"), (1, "D")], (1, "E")),
               (&[(7, "A"), (1, "E")], (1, "FUEL"))]);
         let mut bench = Bench::new();
-        bench.obtain(&book, Thing { quantity: 1, substance: "FUEL".to_owned() });
+        bench.get_fuel(&book);
         assert_eq!(bench.ore_used, 31);
 
         let leftovers: Vec<(String, Amount)> =
@@ -131,7 +158,7 @@ mod test {
               (&[(2, "AB"), (3, "BC"), (4, "CA")], (1, "FUEL"))]);
         
         let mut bench = Bench::new();
-        bench.obtain(&book, Thing { quantity: 1, substance: "FUEL".to_owned() });
+        bench.get_fuel(&book);
         assert_eq!(bench.ore_used, 165);
     }
 
@@ -145,7 +172,7 @@ mod test {
               (&[(1, "AB"), (1, "BA")], (1, "FUEL"))]);
         
         let mut bench = Bench::new();
-        bench.obtain(&book, Thing { quantity: 1, substance: "FUEL".to_owned() });
+        bench.get_fuel(&book);
         assert_eq!(bench.ore_used, 4);
     }
 
@@ -159,8 +186,27 @@ mod test {
               (&[(1, "AB"), (1, "BA")], (1, "FUEL"))]);
  
         let mut bench = Bench::new();
-        bench.obtain(&book, Thing { quantity: 1, substance: "FUEL".to_owned() });
+        bench.get_fuel(&book);
         assert_eq!(bench.ore_used, 6);
+    }
+
+    #[test]
+    fn ex2_parsed() {
+        const TEXT: &[&str] = 
+            &["9 ORE => 2 A",
+              "8 ORE => 3 B",
+              "7 ORE => 5 C",
+              "3 A, 4 B => 1 AB",
+              "5 B, 7 C => 1 BC",
+              "4 C, 1 A => 1 CA",
+              "2 AB, 3 BC, 4 CA => 1 FUEL"];
+        let mut book = Book::new();
+        for &line in TEXT {
+            book.add(Reaction::from_str(line));
+        }
+        let mut bench = Bench::new();
+        bench.get_fuel(&book);
+        assert_eq!(bench.ore_used, 165);
     }
 
 }
