8099edb9cd5e872ed4c5d38df540418da7bef8c4
Jed Davis <jld@xlerb.net>
2019-12-23 22:39:43 -0700

    day24: Jumping ahead because this looks easy.  (Famous last words?)

---
+++ day24/src/main.rs
@@ -0,0 +1,53 @@
+type Grid = u32;
+const ALL: Grid = 0b11111_11111_11111_11111_11111;
+const SHRED: Grid = 0b01111_01111_01111_01111_01111;
+const SHLED: Grid = 0b11110_11110_11110_11110_11110;
+
+fn bugshift(prev: &[Grid], addend: Grid) -> Vec<Grid> {
+    let n = prev.len();
+    let mut next = vec![0; n + 1];
+    for i in 0..n {
+        next[i] |= prev[i] & !addend;
+        next[i+1] |= prev[i] & addend;
+    }
+    next
+}
+
+fn bugadd(planes: &[Grid]) -> Vec<Grid> {
+    planes.iter().fold(vec![ALL], |prev, addend| bugshift(&prev, *addend))
+}
+
+fn neighbors(grid: Grid) -> Vec<Grid> {
+    bugadd(&[(grid >> 1) & SHRED,
+             (grid << 1) & SHLED,
+             grid >> 5,
+             (grid << 5) & ALL])
+}
+
+fn bug_iter(grid: Grid) -> Grid {
+    let counts = neighbors(grid);
+    (grid & counts[1]) | (!grid & (counts[1] | counts[2]))
+}
+
+fn main() {
+    println!("Hello, world!");
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn example() {
+        const TILES: &[Grid] =
+            &[0b00001_00100_11001_01001_10000,
+              0b00110_11011_10111_01111_01001,
+              0b11101_01000_10000_10000_11111,
+              0b10110_01101_11000_01111_00001,
+              0b00011_00000_10011_10000_01111];
+        for i in 1..TILES.len() {
+            assert_eq!(bug_iter(TILES[i-1]), TILES[i]);
+        }
+    }
+}
